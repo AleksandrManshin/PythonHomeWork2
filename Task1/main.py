7d2db5de77734926ca8247013c1761503356697d
Aleksandr <maivan007@yandex.ru>
2022-09-13 18:16:05 +0400

    Add Task1 v2

--- Task1/main.py
+++ Task1/main.py
@@ -2,10 +2,16 @@ import random
 
 quantity = int(input('Введите кол-во монет: '))
 count = 0
+count2 = 0
 
 for i in range(quantity):
     side = random.randint(0, 1)
     print(side, end='\n')
     if side == 0:
         count = count + 1
-print(f"Кол-во монет перевернутых гербом вверх: {count}")
+    else:
+        count2 = count2 + 1
+if count < count2:
+    print(f"Кол-во монет которые нужно перевернуть решкой вверх: {count}")
+else:
+    print(f"Кол-во монет которые нужно перевернуть гербом вверх: {count2}") 
